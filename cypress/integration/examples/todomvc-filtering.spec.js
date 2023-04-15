2f3b8ad91e66d568f26f5a4545dbad02d2ddb78e
GentaZ18 <zabelajgentiana@gmail.com>
2023-04-15 23:13:26 +0200

    filtering test changes

--- cypress/integration/examples/todomvc-filtering.spec.js
+++ cypress/integration/examples/todomvc-filtering.spec.js
@@ -6,6 +6,22 @@ describe('filtering', () => {
         cy.get('.new-todo').type('learn cypress {enter}')
         cy.get('.new-todo').type('clean room {enter}')
         cy.get('.new-todo').type('do skincare {enter}')
+        cy.get(':nth-child(2) > .view > .toggle').click()
+    })
+
+    it('should filter "active" todos', () => {
+        cy.contains('Active').click()
+        cy.get('.todo-list li').should('have.length', 2)
+    })
+
+    it('should filter "completed" todos', () => {
+        cy.contains('Completed').click()
+        cy.get('.todo-list li').should('have.length', 1)
+    })
+
+    it('should filter "all" todos', () => {
+        cy.contains('All').click()
+        cy.get('.todo-list li').should('have.length', 3)
     })
 })
 
