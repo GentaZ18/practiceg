52cce6cc4468097f665659bab6656244d96a8ffd
GentaZ18 <zabelajgentiana@gmail.com>
2023-04-15 15:51:13 +0200

    updates

---
+++ cypress/integration/examples/todomvc-actions.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe('MVC ToDo App actions' ,() =>{
+    beforeEach(() => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+        cy.get('.new-todo').type('learn cypress {enter}')
+    })
+
+it('adds a new item on the list', () => {
+    cy.get('label').should('have.text', 'learn cypress') // making an assertion
+    cy.get('.toggle').should('not.be.checked')
+})
+
+it('checks the item on the list', () => {
+    cy.get('.toggle').click()
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+})
+
+it('clears the completed items', () =>{
+    cy.get('.toggle').click()
+    cy.contains('Clear completed').click() //we can use it instead of cy.get
+    cy.get('.todo-list').should('not.have.descendants', 'li')
+})
+}
+)
